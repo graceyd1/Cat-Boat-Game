5bec7fbcdc5540e3f8207aa98e01563a53f20c46
grace <graceyding@gmail.com>
2026-06-09 19:47:06 -0500

    Added first room sprites and walls

--- FishEnemy.cs
+++ FishEnemy.cs
@@ -6,6 +6,9 @@ public partial class FishEnemy : Node2D
 	[Export]
 	public int Speed{get; set;} = 60;
 
+	[Export]
+	public Boolean isOrange {get; set;} = true;
+
 	private static int[] Velocities = {1, 0, -1, 0};
 
 	private int idx;
@@ -14,6 +17,8 @@ public partial class FishEnemy : Node2D
 	public override void _Ready()
 	{
 		idx = 0;
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		animatedSprite2D.Play();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,7 +30,32 @@ public partial class FishEnemy : Node2D
 
 		Position += velocity * (float)delta;
 
-		//animation (add later)
+		//animation (set isOrange in the Inspector)
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		if (isOrange)
+		{
+			animatedSprite2D.Animation = "orange";
+		}
+		else
+		{
+			animatedSprite2D.Animation = "pink";
+		}
+
+		if (velocity.X < 0)
+		{
+			animatedSprite2D.FlipH = true;
+			animatedSprite2D.Play();
+		}
+		else if (velocity.X > 0)
+		{
+			animatedSprite2D.FlipH = false;
+			animatedSprite2D.Play();
+		}
+		else
+		{
+			animatedSprite2D.Stop();
+			//animatedSprite2D.FlipH = !animatedSprite2D.FlipH;
+		}
 	}
 
 	private void OnTimerTimeout()
