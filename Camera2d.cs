1ba0c20b23ce8af30fd9259ac988d9b19877653e
grace <graceyding@gmail.com>
2026-06-11 11:14:35 -0500

    Added enter cave room, growable vine sprites, and toggle flashlight with F

--- Camera2d.cs
+++ Camera2d.cs
@@ -7,12 +7,8 @@ public partial class Camera2d : Camera2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
 		if (GetParent()?.GetParent().Name == "FirstRoom") {
+			Enabled = true;
 			SetLimit(Side.Left, 0);
 			SetLimit(Side.Right, 500);
 			SetLimit(Side.Top, 0);
@@ -21,10 +17,21 @@ public partial class Camera2d : Camera2D
 
 		//temporary
 		if (GetParent()?.GetParent().Name == "TestingRoom") {
+			Enabled = true;
 			SetLimit(Side.Left, 0);
 			SetLimit(Side.Right, 500);
 			SetLimit(Side.Top, 0);
 			SetLimit(Side.Bottom, 180);
 		}
+
+		if (GetParent()?.GetParent().Name == "EnterCaveRoom")
+		{
+			Enabled = false; //todo - fix this (heart is not on top of screen)
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
 	}
 }
--- Flashlight.cs
+++ Flashlight.cs
@@ -11,12 +11,20 @@ public partial class Flashlight : PointLight2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var mousePos = GetGlobalMousePosition();
-		var characterPos = GlobalPosition;
+		if (Input.IsActionJustPressed("toggle_flashlight"))
+		{
+			Enabled = !Enabled;
+		}
 
-		//get the angle from the character to the mouse
-		var angle = Math.Atan2(mousePos.Y - characterPos.Y, mousePos.X - characterPos.X);
+		if (Enabled)
+		{
+			var mousePos = GetGlobalMousePosition();
+			var characterPos = GlobalPosition;
 
-		Rotation = (float) angle;
+			//get the angle from the character to the mouse
+			var angle = Math.Atan2(mousePos.Y - characterPos.Y, mousePos.X - characterPos.X);
+
+			Rotation = (float) angle;
+		}
 	}
 }
